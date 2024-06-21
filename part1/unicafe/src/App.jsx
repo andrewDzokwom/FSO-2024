b75199836270e8b244f0b406143f15d15babe474
andrewdzokwom <andrewa441@gmail.com>
2024-06-21 23:31:47 +0100

    solve 1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,33 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react"
+
+const Statistic = (props)=>{
+
+  
+  const {good, neutral, bad} = props
+  const total = good + neutral + bad
+  if (total === 0){
+    return <p>No feedback given</p>
+  }
+
+  console.log(props)
+  return (
+    <>
+    
+    <p>good: {good}</p>
+    <p>neutral {neutral}</p>
+    <p>bad {bad}</p>
+    <p>all {good+bad+neutral}</p>
+    <p>average{((good - bad)/(good + bad + neutral))}</p>
+    <p>positive {(good/(good + bad + neutral)*100)}%</p>
+  </>
+  )
+}
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  
+  
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <h2>give feedback</h2>
+    <div>
+      <button onClick={()=> setGood(good + 1)}>good</button>
+      <button onClick={()=> setNeutral(neutral + 1)}>neutral</button>
+      <button onClick={()=> setBad(bad + 1)}>bad</button>
+    </div>
+    <h2>statistics</h2>
+    <Statistic good={good} bad={bad} neutral={neutral} />
+
     </>
   )
 }
