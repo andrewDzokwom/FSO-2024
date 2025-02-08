a8b07107ec0fb2f184e6a55e893cf4dba09619cd
andrewDzokwom <andrewa441@gmail.com>
2025-02-08 21:17:29 +0100

    set up MongoDB connection and environment configuration

--- part4/app.js
+++ part4/app.js
@@ -1,5 +1,25 @@
 const express = require('express')
 const cors = require('cors')
+const mongoose = require('mongoose')
+const config  = require('./utils/config')
+
+const blogSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number,
+})
+
+const Blog = mongoose.model('Blog', blogSchema)
+mongoose.connect(config.MONGODB_URI)
+  .then(()=>{
+    console.log('Connected successfully!');
+    
+  })
+  .catch((error)=>{
+    console.log(error.message);
+    console.log('failed to connect!');
+  })
 
 
 const app = express()
@@ -7,11 +27,13 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.get('/', (req, res)=>{
-  res.send('This is to  prove  that the server is communicating with the backend')
+  res.json({
+    message: "This is working!"
+  })
 })
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, ()=>{
-  console.log(`Server running on address http://locashost:${PORT}`);
+  console.log(`Server running on address http://localhost:${PORT}`);
   
 })
